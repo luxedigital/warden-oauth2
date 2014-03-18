786889ef9a620db775aa7b418deb592b6d720cd0
Bastien Vetault <bastien.vetault@gmail.com>
2014-03-18 17:24:59 +1100

    Fixing rspec deprecation warnings

--- spec/warden/oauth2/strategies/bearer_spec.rb
+++ spec/warden/oauth2/strategies/bearer_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Warden::OAuth2::Strategies::Bearer do
   let(:strategy){ Warden::OAuth2::Strategies::Bearer }
-  let(:token_model){ mock(:AccessToken) }
+  let(:token_model){ double(:AccessToken) }
   subject{ strategy.new({'rack.input' => {}}) }
 
   before do
@@ -12,20 +12,20 @@ describe Warden::OAuth2::Strategies::Bearer do
   describe '#token_string_from_header' do
     Rack::Auth::AbstractRequest::AUTHORIZATION_KEYS.each do |key|
       it "should recognize a bearer token in the #{key} environment key" do
-        subject.stub!(:env).and_return({key => "Bearer abc"})
+        subject.stub(:env).and_return({key => "Bearer abc"})
         subject.token_string_from_header.should == 'abc'
       end
     end
 
     it 'should ignore a non-bearer authorization header' do
-      subject.stub!(:env).and_return('HTTP_AUTHORIZATION' => 'Other do do do')
+      subject.stub(:env).and_return('HTTP_AUTHORIZATION' => 'Other do do do')
       subject.token_string_from_header.should be_nil
     end
   end
 
   describe '#token_string_from_request_params' do
     it 'should pull the :access_token param' do
-      subject.stub!(:params).and_return(:access_token => 'abc')
+      subject.stub(:params).and_return(:access_token => 'abc')
       subject.token_string_from_request_params.should == 'abc'
     end
   end
